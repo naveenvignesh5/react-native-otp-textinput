3bb9d262c8bf24b4b94f669021fd934dacb8641f
BV <bv@apples-MacBook-Pro.local>
2018-07-12 13:11:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### REACT NATIVE OTP TEXT INPUT
+
+React Native Component that can used for OTPs and Pins as secure pin input.
+
+##### Platform Support
+Supports both Android and iOS.
+
+##### Props
+The following props are applicable for the component.
+Prop              | Type     | Optional | Default     | Description
+----------------- | -------- | -------- | ----------- | -----------
+defaultValue         | string     | Yes       | ''       | Default Value that can be set based on OTP / Pin received from parent container.
+handleTextChange         | func     | No       |  n/a      | callback with concated string of all cells as argument.
+inputCount          | number      | Yes      | 4        | Number of Text Input Cells to be present.
+tintColor          | string     | Yes      | #3CB371        | Color for Cell Border on being focused.
+offTintColor       | string     | Yes      | #DCDCDC | Color for Cell Border Border not focused.
+cellTextLength       | number     | Yes      | 1 | Number of character that can be entered inside a single cell.
+containerStyle       | object     | Yes      | {} | style for overall container.
+textInputStyle       | object     | Yes      | {} | style for text input.
+textStyle       | object     | Yes      | {} | style for text inside text input.
+
+### TO-DO
+- [ ] Unit Testing
+- [ ] Optimizing Code
---
+++ index.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import { View, TextInput, StyleSheet, Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    textInput: {
+        height: 50,
+        width: 50,
+        borderBottomWidth: 4,
+        margin: 5,
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 22,
+        fontWeight: '500',
+        color: '#000000'
+    }
+});
+
+const window = Dimensions.get('window');
+
+class OTPTextView extends Component {
+    state = {
+        focusedInput: 0,
+    }
+
+    componentDidMount() {
+        const { defaultValue, cellTextLength } = this.props;
+        if (defaultValue) {
+            this.otpText = defaultValue.match(new RegExp('.{1,' + cellTextLength + '}', 'g'));
+        }
+    }
+
+    otpText = [];
+
+    onTextChange = (text, i) => {
+        const { cellTextLength, inputCount, handleTextChange } = this.props;
+        this.otpText[i] = text;
+        handleTextChange(this.otpText.join(""));
+        if (text.length === cellTextLength && i !== inputCount - 1) {
+            this.refs[`${i + 1}`].focus();
+        }
+    }
+
+    onInputFocus = (i) => {
+        this.setState({ focusedInput: i });
+    }
+
+    render() {
+        const {
+            inputCount,
+            offTintColor,
+            tintColor,
+            defaultValue,
+            cellTextLength,
+            containerStyle,
+            textInputStyle,
+            textStyle,
+            ...textInputProps
+        } = this.props;
+
+        const TextInputs = [];
+
+        for (let i = 0; i < inputCount; i += 1) {
+            let defaultChars = [];
+            if (defaultValue) {
+                defaultChars = defaultValue.match(new RegExp('.{1,' + cellTextLength + '}', 'g'));
+            }
+            const inputStyle = [
+                styles.textInput,
+                styles.text,
+                textInputStyle,
+                textStyle,
+                { borderColor: offTintColor }
+            ];
+            if (this.state.focusedInput === i) {
+                inputStyle.push({ borderColor: tintColor });
+            }
+
+            TextInputs.push(
+                <TextInput
+                    ref={`${i}`}
+                    key={i}
+                    defaultValue={defaultValue ? defaultChars[i] : ''}
+                    style={inputStyle}
+                    maxLength={this.props.cellTextLength}
+                    onFocus={() => this.onInputFocus(i)}
+                    onChangeText={(text) => this.onTextChange(text, i)}
+                    multiline={false}
+                    {...textInputProps}
+                />
+            );
+        }
+        return (
+            <View style={[styles.container, containerStyle]}>
+                {TextInputs}
+            </View>
+        );
+    }
+}
+
+OTPTextView.propTypes = {
+    defaultValue: PropTypes.string,
+    inputCount: PropTypes.number,
+    containerStyle: PropTypes.object,
+    textInputStyle: PropTypes.object,
+    cellTextLength: PropTypes.number,
+    tintColor: PropTypes.string,
+    offTintColor: PropTypes.string,
+    handleTextChange: PropTypes.func,
+    inputType: PropTypes.string,
+}
+
+OTPTextView.defaultProps = {
+    defaultValue: '',
+    inputCount: 4,
+    tintColor: '#3CB371',
+    offTintColor: '#DCDCDC',
+    cellTextLength: 1,
+    containerStyle: {},
+    textInputStyle: {},
+    textStyle: {},
+    handleTextChange: () => { },
+}
+
+export default OTPTextView;
